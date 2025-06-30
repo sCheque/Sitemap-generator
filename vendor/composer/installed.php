35d2af23808dd15bccfcc5e927e69b0888cfb0cc
nik <125746555+sCheque@users.noreply.github.com>
2025-06-30 15:29:22 +0400

    fixed composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'scheque/sitemap-generator',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '63ecb5bca97c754ee7f7f7609ba2f21a7e9758a4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'scheque/sitemap-generator' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '63ecb5bca97c754ee7f7f7609ba2f21a7e9758a4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
